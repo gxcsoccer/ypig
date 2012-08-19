9f2e97abb43c00bd3cdbafcbc568feee49994774
gaoxiaochen <gxcsoccer@126.com>
2012-08-19 15:08:49 +0800

    init

---
+++ server.js
@@ -0,0 +1,20 @@
+/**
+ * @author XIAOCHEN GAO
+ */
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(9527);
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/static/client.html');
+});
+
+io.sockets.on('connection', function(socket) {
+	console.log('new client connected');
+});
